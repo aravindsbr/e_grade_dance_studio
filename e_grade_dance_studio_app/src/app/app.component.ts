91d9761b8963e871640fa590ddc28b347ce40f9f
aravinds <aravindsbr@gmail.com>
2023-06-11 15:36:52 +0530

    added service page css and js

--- e_grade_dance_studio_app/src/app/app.component.ts
+++ e_grade_dance_studio_app/src/app/app.component.ts
@@ -7,4 +7,26 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'e_grade_dance_studio_app';
+  isClasses: boolean = true;
+  listOfClasses = [
+    '../assets/images/class_1.png',
+    '../assets/images/class_2.png',
+    '../assets/images/class_3.png',
+    '../assets/images/class_4.png',
+    '../assets/images/class_5.png',
+    '../assets/images/class_6.png',
+    '../assets/images/class_7.png',
+  ];
+  listOfEvents = [
+    '../assets/images/event_1.png',
+    '../assets/images/event_2.png',
+    '../assets/images/event_3.png',
+    '../assets/images/event_4.png',
+    '../assets/images/event_5.png',
+    '../assets/images/event_6.png',
+    '../assets/images/event_7.png',
+  ];
+  toggleService(serviceType: boolean) {
+    this.isClasses = serviceType;
+  }
 }
